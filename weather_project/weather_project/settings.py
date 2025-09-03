64d3d40a3498ba28ba65b869b8d8a20a9160a56d
Hritika <hritikasahu07@gmail.com>
2025-09-04 01:10:48 +0530

    update

--- weather_project/weather_project/settings.py
+++ weather_project/weather_project/settings.py
@@ -118,9 +118,7 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
